361afbad232c67837b7388998e943d091e0fb8b5
cjbellmyer <cjbellmyer5984@gmail.com>
2023-12-06 15:21:25 -0600

    scroll disappearing header implemented

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Header = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    const visible = prevScrollPos > currentScrollPos;
+
+    setPrevScrollPos(currentScrollPos);
+    setVisible(visible)
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
-    <header className='header'>
+    <header className={`header ${visible ? 'visible' : ''}`}>
         header
     </header>
   )
